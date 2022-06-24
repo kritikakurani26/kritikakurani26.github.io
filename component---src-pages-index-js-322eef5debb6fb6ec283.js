ca1b426339d89e2d84bbfa7e1032d3e78191450c
kritikakurani26 <kritikakurani@gmail.com>
2022-06-24 07:27:38 -0700

    Updates

---
+++ component---src-pages-index-js-322eef5debb6fb6ec283.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{8296:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var r=n(7294),a=n(1463),l=n(262),u=n(6407),i=function(){return r.createElement(a.Z,null,r.createElement(l.Z,{title:"Kritika Kurani"}),r.createElement("div",{className:"index-module--textCenter--Ts42-"},r.createElement(u.Z,null,"Hi! I'm Kritika."),r.createElement(u.Z,null,"A Software Engineer")))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-322eef5debb6fb6ec283.js.map
